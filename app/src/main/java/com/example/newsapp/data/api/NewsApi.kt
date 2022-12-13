02bcdc6031f5108a83acd2386d337f043a420388
Misa <milosursulovic2696@gmail.com>
2022-12-13 10:05:49 +0100

    Fix annotation in news api interface

--- app/src/main/java/com/example/newsapp/data/api/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/data/api/NewsApi.kt
@@ -14,7 +14,7 @@ interface NewsApi {
         @Query("apiKey") apiKey: String = BuildConfig.API_KEY
     ): Response<NewsResponse>
 
-    @GET("/v2/top-headlines")
+    @GET("/v2/everything")
     suspend fun searchForNews(
         @Query("q") searchQuery: String,
         @Query("page") pageNumber: Int = 1,
